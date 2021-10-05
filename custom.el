d393c7d764ee23cccb60fa95d305ad344da8cb97
nikugronberg <niku.gronberg@student.lut.fi>
2021-10-05 21:55:48 +0300

    init

---
+++ custom.el
@@ -0,0 +1,53 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
+ '(custom-safe-themes
+   '("05604fd187db30368023d71e9d96d4073ec4beeaa2050ca8699d2b50be7a4040" "c3595dd3ec6ebc2651127d3452b357dfc48613a4806e0630f288ce62cb068f54" "311c7d771cd68e549763aa78f968dd12e0fc103626a7b20d41726348c9c88f88" "6ac168552a849fb997ca06a77c854f3a73247fbfe3d4530bcac731a6f496bc1d" "860a2d0be28108334c92c07ad4293ad8b4138ef5d5af700e754974401a9ad753" "5a343f93cded62690a8167421249b7ceea1cca89224ad3550e85006305344ca0" "9510a17ae5fbdc32fae66bb65630ee476d8cf4a1decffadfe95fda690690c667" "22d4cde5fa44a89b4dfd3640624bc32ed4aa9c652d4c152c3baa7bbe156ab1c5" "0bf90313708d72b00c8ab09bbc9f0d57e764fd208e93bc464be55a930ee90028" "5fbf5a7b491ebcbb8bc7b7641434adb8fea411b8739dfd2ea3a418a7bb320832" "5e30a70fef753b5b0f40f1af8bfb9cbb77182e45f8f56b81830bfcaf41d81e43" "8c25dad5cb3d173d66932ad5ae56019e8ae68235df620bb5854bae40c7174048" "129b0ba113d3acd746e976162369d9ec38cf19371c0c605aa729f4bf6b9cef92" "778fadea1fd0f6a4ea801ef59d4de0b88c58b1b793fadee70888829457bd6f10" "7354d92a4dde1b8071a0b21937e3d792abd956b983530d27788acbda0eb9b9d3" "fde90e714bf2b1a55325bd82cdc15b2141555263340b3a2fcbd7b72fe73671d9" "34235be96d680dee818d58371286aed3c13441f287156bb03f302c46637bcfca" "5810e424b9f7cadc9ef956e5ec6c375e814da3306186c0f3ab459abbbabcc76a" "b732f05b0391fcb3b50e8b79d78c6b903fac7cc894d528eb06408d9bc6da5e67" default))
+ '(doom-big-font-mode t)
+ '(exwm-floating-border-color "#1c1f24")
+ '(fci-rule-color "#62686E")
+ '(global-display-fill-column-indicator-mode t)
+ '(highlight-tail-colors
+   ((("#2c3636" "#99bb66" "green")
+     . 0)
+    (("#293b44" "#46D9FF" "brightcyan")
+     . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#1c1f24" "#51afef"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1c1f24" "#7bc275"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1c1f24" "#484854"))
+ '(objed-cursor-color "#ff665c")
+ '(pdf-view-midnight-colors (cons "#bbc2cf" "#242730"))
+ '(rustic-ansi-faces
+   ["#242730" "#ff665c" "#7bc275" "#FCCE7B" "#51afef" "#C57BDB" "#5cEfFF" "#bbc2cf"])
+ '(vc-annotate-background "#242730")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#7bc275")
+    (cons 40 "#a6c677")
+    (cons 60 "#d1ca79")
+    (cons 80 "#FCCE7B")
+    (cons 100 "#f4b96e")
+    (cons 120 "#eda461")
+    (cons 140 "#e69055")
+    (cons 160 "#db8981")
+    (cons 180 "#d082ae")
+    (cons 200 "#C57BDB")
+    (cons 220 "#d874b0")
+    (cons 240 "#eb6d86")
+    (cons 260 "#ff665c")
+    (cons 280 "#d15e59")
+    (cons 300 "#a35758")
+    (cons 320 "#754f56")
+    (cons 340 "#62686E")
+    (cons 360 "#62686E")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
